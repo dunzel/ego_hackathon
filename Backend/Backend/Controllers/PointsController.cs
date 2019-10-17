8827857f5f8d9449d38ddb5f055069fc69084574
DestinyBlade <54405753+DestinyBlade@users.noreply.github.com>
2019-10-18 00:35:05 +0200

    Added Points Controller.cs

---
+++ Backend/Backend/Controllers/PointsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Backend.Controllers
+{
+    public class PointsController : ApiController
+    {
+        // GET: api/Points
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Points/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Points
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Points/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Points/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
