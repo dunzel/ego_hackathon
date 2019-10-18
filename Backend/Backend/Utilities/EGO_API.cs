032326d24151a1906f38df12e67cdf99da98147e
JakobMiesner <jakem@gmx.de>
2019-10-18 02:03:28 +0200

    Utilities Folder added

---
+++ Backend/Backend/Utilities/EGO_API.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Utilities
+{
+    public class EGO_API
+    {
+        public ushort getTotalCars()
+        {
+
+        }
+    }
+}
